925be02017d8c5629b7af53d87da3374c215694a
BalashovDV <korochun@yandex.ru>
2023-12-14 19:03:50 +0300

    Интернет магазин. Домашние работы по курсу "Работа с ORM в Django". Задания по уроку 20.1. Выполнено Задание 2. - cозданы модели Product и Category; - созданы заглушки для __str__ и Meta.

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+	
+	def __str__(self):
+		# Строковое отображение продукта
+		return f'{"product1"} {"product2"}'
+	
+	class Meta:
+		# Начальные настройки?
+		pass
+
+
+class Category(models.Model):
+	
+	def __str__(self):
+		# Строковое отображение категории
+		return f'{"category1"} {"category2"}'
+		
+	class Meta:
+		# Начальные настройки?
+		pass
