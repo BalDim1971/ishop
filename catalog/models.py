09943a0274e1981ba79a2d8807ad3bc75a91a532
BalashovDV <korochun@yandex.ru>
2023-12-15 06:19:25 +0300

    Интернет магазин. Домашние работы по курсу "Работа с ORM в Django". Задания по уроку 20.1. Предварительно Задание 3. - описаны поля в моделях Product и Category; - установлена библиотека Pillow, с добавлением в requirements.txt.

--- catalog/models.py
+++ catalog/models.py
@@ -4,10 +4,21 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
+	'''
+	Класс-модель, описывающий некий продукт.
+	'''
+	
+	name = models.CharField(max_length=50, verbose_name='наименование')
+	description = models.TextField(verbose_name='описание')
+	preview = models.ImageField(verbose_name='превью')
+	category = models.ForeignKey('Category', on_delete=models.PROTECT)
+	price = models.IntegerField(verbose_name='цена за штуку')
+	date_create = models.DateTimeField(verbose_name='время создания')
+	date_last_change = models.DateTimeField(verbose_name='время последнего изменения')
 	
 	def __str__(self):
 		# Строковое отображение продукта
-		return f'{"product1"} {"product2"}'
+		return f'{self.name} {self.description}'
 	
 	class Meta:
 		# Начальные настройки?
@@ -15,10 +26,16 @@ class Product(models.Model):
 
 
 class Category(models.Model):
+	'''
+	Класс-модель, описывающий некоторую категорию продуктов.
+	'''
+	
+	name = models.CharField(max_length=50, verbose_name='наименование', db_index=True)
+	description = models.TextField(verbose_name='описание')
 	
 	def __str__(self):
 		# Строковое отображение категории
-		return f'{"category1"} {"category2"}'
+		return f'{self.name} {self.description}'
 		
 	class Meta:
 		# Начальные настройки?
