a839634f316f4bf82ccec5e523ea73574a058e64
BalashovDV <korochun@yandex.ru>
2023-12-18 06:18:07 +0300

    Интернет магазин. Домашние работы по курсу "Работа с ORM в Django". Домашнее задания по уроку 20.2. 1. Выделен шаблон для обратной связи 2. Добавлена загрузка статики в страницах.

--- catalog/views.py
+++ catalog/views.py
@@ -2,18 +2,13 @@ from django.shortcuts import render
 
 
 def index(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'You have new message from {name}({email}): {message}')
-    return render(request, 'main/index.html')
+	return render(request, 'catalog/index.html')
 
 
 def contact(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
-        print(f'You have new message from {name}({email}): {message}')
-    return render(request, 'main/contact.html')
+	if request.method == 'POST':
+		name = request.POST.get('name')
+		email = request.POST.get('email')
+		message = request.POST.get('message')
+		print(f'You have new message from {name}({email}): {message}')
+	return render(request, 'catalog/contacts.html')
