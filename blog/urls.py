3cd173ee089a30f2fee79bc654dccafc40b47840
Дмитрий Балашов <korochun@yandex.ru>
2023-12-20 14:07:33 +0300

    Курс 6. Django Урок 21.1 FBV и CBV. Домашнее задание Добавлены представления для приложения Блог. Задания 2 и 3. Пишем CRUD. Разрабатываем запросы на уровне контроллеров.

--- blog/urls.py
+++ blog/urls.py
@@ -6,13 +6,14 @@ URL configuration для приложения blog.
 from django.urls import path
 
 from blog.apps import BlogConfig
+from blog.views import BlogCreateView
 
 
 app_name = BlogConfig.name
 
 urlpatterns = [
+    path('', BlogListView.as_view(), name='list'),
     path('create/', BlogCreateView.as_view(), name='create'),
-    path('create/', ..., name='list'),
     path('view/<int:pk>', ..., name='view'),
     path('edit/<int:pk>', ..., name='edit'),
     path('delete/<int:pk>', ..., name='delete'),
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,34 @@
-from django.views.generic import CreateView
+'''
+Представления для задачи Блог
+'''
+
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views.generic import CreateView, DeleteView, ListView, DetailView, UpdateView
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import render
+from pytils.translit import slugify
 
 from blog.models import Blog
 
 
-class BlogCreteView(CreateView):
+class BlogCreateView(CreateView):
 	model = Blog
-	fields = ('title', 'slug', 'body',)
+	fields = ('title', 'body',)
+	success_url = reverse_lazy('blog:list')
+
+	def form_valid(self, form):
+		if form.is_valid():
+			new_blog = form.save()
+			new_blog.slug = slugify(new_blog.title)
+			new_blog.save()
+		return super().form_valid(form)
+
+class BlogListView(LoginRequiredMixin, ListView):
+	model = Blog
+
+	def get_queryset(self, *args, **kwargs):
+		queryset = super().get_queryset(*args, **kwargs)
+		queryset = queryset.filter(is_published=True)
+
+		return queryset
 	
